64656fa69a67c9b9bbbb1416279d27792984800f
Taavi Metso <tade.man98@gmail.com>
2022-03-28 14:34:38 +0300

    Siirretty mainiin

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
+import math
 import sys
 import pygame
-def piirrä(näyttö):
-    pygame.draw.circle(näyttö, (200, 200, 0), (250, 250), 80)
+def piirrä(näyttö,aika):
+    näyttö.fill((255, 255, 255))
+    pygame.draw.circle(näyttö, (200, 000, 0), (250, 250), (math.sin(aika/20)+1)*100)
     pygame.display.flip()
     pass
-pygame.init()  
-scr = pygame.display.set_mode((600,500))  
-pygame.display.set_caption('Pygame Window')
-while True:
-    # Näppäimet
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
+def main():
+    pygame.init()
+    scr = pygame.display.set_mode((600, 500))
+    pygame.display.set_caption('Pygame Window')
+    clock = pygame.time.Clock()
+    Hiiri = [0, 0, 0]
+    aika = 0
+    while True:
+        # Näppäimet
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_s:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    pass
+            if event.type == pygame.KEYUP:
                 pass
-        if event.type == pygame.KEYUP:
-            pass
-    piirrä(scr)
+        piirrä(scr,aika)
+        aika += 1
+        clock.tick(60)
+        if pygame.mouse.get_pressed()[0]:
+            if Hiiri[0]:
+                Hiiri[0] = 0
+                pass
+        else:
+            Hiiri[0] = 1
+
+if __name__ == "__main__":
+    main()
+
 
